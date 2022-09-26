593953b37c2ff4a4e0f8c2ad03df0ffcc8abba99
LDaqiang <LDaqiang@live.com>
2022-09-26 09:15:55 +0800

    taro原生组件

--- src/pages/order/index.vue
+++ src/pages/order/index.vue
@@ -1,16 +1,20 @@
 <template>
     <swiper class='swiper-item' indicator-color='#999' indicator-active-color='#333' :vertical="false" :circular="true"
         :indicator-dots="true" :autoplay="true">
-        <swiper-item>
+        <swiper-item v-for="item in swiperImgs" :key="item">
             <view class='demo-text-1'>
-                <image mode="heightFix" lazyLoad="true" src="@/assets/images/fristfabu.jpg" alt="" />
-            </view>
-        </swiper-item>
-        <swiper-item>
-            <view class='demo-text-1'>
-                <image mode="heightFix" lazyLoad="true" src="@/assets/images/fristfabu.jpg" alt="" />
+                <image mode="heightFix" lazyLoad="true" :src='item' alt="" />
             </view>
         </swiper-item>
     </swiper>
 </template>
 
+<script lang="ts" setup>
+
+const swiperImgs: string[] = [
+    '/assets/images/fristfabu.jpg',
+    '/assets/images/fristfabu.jpg',
+    '/assets/images/fristfabu.jpg',
+    '/assets/images/fristfabu.jpg',
+]
+</script>
